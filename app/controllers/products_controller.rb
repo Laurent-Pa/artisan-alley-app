e5430ddc5dd7617ba5cffe59ac227babb8a072af
Aurelien Thery <aurelien.thery59@gmail.com>
2025-03-04 17:28:31 +0100

    Add dotenv-rails

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,7 +38,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :adress)
+    params.require(:product).permit(:name, :description, :price, :adress, photos: [])
   end
 
   def set_params
