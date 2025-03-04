59245bf7fd64ca1e893919b7bf8b4945f4839097
laurent papinaud <laurent.papinaud@gmail.com>
2025-03-04 13:56:45 +0100

    seeds created (10 artisans, 5 products each

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,46 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+### READ THIS IF YOU WANT TO ADD FAKE, USERS, PRODUCTS and ORDERS
+
+# You must add gem faker to gemfile and run a bundle install
+
+# Uncomment theses ligns according to want you want to create
+# require 'faker'
+# Faker::Config.locale = 'fr' # to have a french cellphone number
+
+# 1. we create fake artisans/users (because a product belongs to a user)
+puts "creating 10 fake artisans"
+10.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = "#{first_name.downcase}.#{last_name.downcase}@artisan.fr"
+
+  artisan = User.create(
+    first_name: first_name,
+    last_name: last_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    user_type: "artisan",
+    email: email,
+    encrypted_password: "xxx"
+  )
+  puts "user #{first_name.downcase}.#{last_name.downcase} created"
+
+  # 2. we create fake products /!\ the model product must be created
+  puts "creation des produits de cet artisan"
+  5.times do
+    artisan_id = artisan.id
+    quantity = (1..5)
+    Product.create(
+      artisan_id: artisan_id,
+      name: Faker::Book.title,
+      stock_quantity: quantity,
+      adress: "107 Cr Balguerie Stuttenberg, 33300 Bordeaux",
+      description: Faker::Quote.yoda,
+      price: (0..200)
+    )
+    puts "5 products created!"
+  end
+end
+puts "you have created 10 fake artisans with 5 for products each"
