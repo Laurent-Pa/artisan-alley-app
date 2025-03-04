654c1876420c1ae663505208de16d93f7853c40a
laurent papinaud <laurent.papinaud@gmail.com>
2025-03-04 17:19:53 +0100

    User sign up form updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # a décommenter dès que la base de donnée est créée
+  #has_many :products
+
+  # enumerable pour stocker les types d'utilisateurs (choix lors du sign up)
+  # /!\ stocké en integer dans bdd donc ds table user: t.integer "user_type"
+  enum user_type: %i[artisan customer]
+
+  # on indique en validation du user_type la liste des clés de user_typeS
+  validates :user_type, presence: true, inclusion: { in: User.user_types.keys }
 end
