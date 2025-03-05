62389408f0b2389bfd10dcadd34d71f1e79e4283
stephane sampah gh api stefsampah@hotmail.com <stefsampah@hotmail.com>
2025-03-05 14:32:41 +0100

    conflit fixe

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-<<<<<<< HEAD
+
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :orders
-=======
   resources :products
->>>>>>> 88cba7677cf8f5d091e45f76a0a4e9d37311e03f
+
 end
